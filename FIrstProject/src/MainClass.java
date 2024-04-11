17188f4da2545bcdc336a663a94145c45cff4165
sist113_1 <sist113_1@sist113>
2024-04-11 12:32:15 +0900

    1111

--- FIrstProject/src/MainClass.java
+++ FIrstProject/src/MainClass.java
@@ -1,11 +1,12 @@
-
+/*
+ 	여러 줄 주석
+ */
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 한줄주석
 		System.out.println("Hello Java");
 		
-		////
 	}
 
 }
