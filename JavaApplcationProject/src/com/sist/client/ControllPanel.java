65cbec0c941f8a31bb3d342cd109fe0ff4ab6d42
sist113_1 <sist113_1@sist113>
2024-06-11 17:10:38 +0900

    123

---
+++ JavaApplcationProject/src/com/sist/client/ControllPanel.java
@@ -0,0 +1,15 @@
+
+import java.awt.CardLayout;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class ControllPanel extends JPanel{
+   CardLayout card=new CardLayout();
+   EmpListPanel ep=new EmpListPanel();
+   public ControllPanel()
+   {
+	   setLayout(card);
+	   add("EP",ep);
+   }
+}
